21f26f625989b6fdb8e769ab6b1b7c5f7dcb2ff4
Greg Schuman <gcschuman@hotmail.com>
2022-08-02 13:18:30 -0700

    Ternary expression that decides what to do if the user enters a name or not

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // Variable of a user's name, set to an empty string since it will be changing
 let userName = "Greg";
 
-// Ternary expression
+// Ternary expression that decides what to do if the user enters a name or not
+userName ? console.log(`Hello, ${userName}!`) : console.log("Hello")
+
+// 
