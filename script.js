65f189552f38c251bcc9b07d44e84fd0d68a0eaa
Greg Schuman <gcschuman@hotmail.com>
2022-08-02 13:31:25 -0700

    Added the control flow for answers to questions.

--- script.js
+++ script.js
@@ -19,7 +19,33 @@ console.log(randomNumber)
 // Variable of eight ball
 let eightBall = ""
 
-
+// Control flow that takes in randomNumber and assigns eightBall to a reply the Magic Eight Ball will return
+switch(randomNumber){
+    case 0:
+      eightBall = 'It is certain';
+      break
+    case 1:
+      eightBall = 'It is decidedly so';
+      break
+    case 2:
+      eightBall = 'Reply hazy, try again';
+      break
+    case 3:
+      eightBall = 'Cannot predict now';
+      break
+    case 4:
+      eightBall = 'Dont count on it';
+      break
+    case 5:
+      eightBall = 'My sources say no';
+      break
+    case 6:
+      eightBall = 'Outlook not so good';
+      break
+    case 7:
+      eightBall = 'All signs point to yes';
+      break
+  }
 
 
 
